78ff47e8a9c7fad3b15de6f6fb8c0f3b6a24f614
Osama Samy <149599819+Osama-Samy@users.noreply.github.com>
2025-02-20 18:37:18 +0200

    Update task.routes.js

--- src/modules/task/task.routes.js
+++ src/modules/task/task.routes.js
@@ -8,10 +8,10 @@ taskRouter.post('/add', verifyToken, addTask)
 
 taskRouter.get('/all', verifyToken, getAllTasks)
 
-taskRouter.put('/update/:id', verifyToken, updateTask)
+taskRouter.put('/update/:taskId', verifyToken, updateTask)
 
-taskRouter.delete('/delete/:id', verifyToken, deleteTask)
+taskRouter.delete('/delete/:taskId', verifyToken, deleteTask)
 
-taskRouter.patch('/patch/:id', verifyToken, updateStatus)
+taskRouter.patch('/patch/:taskId', verifyToken, updateStatus)
 
 export default taskRouter
